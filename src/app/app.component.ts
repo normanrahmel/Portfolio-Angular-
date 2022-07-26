e17a34739bd899cf2234b4e90f32ed5072cde62b
normanrahmel <92368615+normanrahmel@users.noreply.github.com>
2022-07-26 15:49:46 +0200

    newPics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'normanrahmel.de';
+export class AppComponent implements OnInit {
+
+ images =['analytics.png','connection.png','hero.png'];
+
+ currentImage=0;
+
+showImage=true;
+
+
+ ngOnInit(){
+  this.upDateImage();
+ }
+
+
+ upDateImage(){
+
+  setInterval(() => {
+    this.currentImage++;
+    this.currentImage = this.currentImage % this.images.length;
+    this.showImage = false;
+    setTimeout(() => {
+      this.showImage=true;
+    }, 1);
+ }, 8000); 
+}
+
+
 }
